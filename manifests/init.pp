c863708892f62c5974cda94b1813529a359ed134
Johan Wennerberg <johan.x.wennerberg@ericsson.com>
2014-04-09 10:50:01 +0200

    Add validation and style fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,15 +1,30 @@
+# == Class: infiniband
+#
 class infiniband (
-  $map_ib='true',
-  $ibnetworks={},
-  $ibnetmask='255.255.252.0',
-  $ibhosts={},
-  $ibinterface='ib0'
+  $map_ib      = true,
+  $ibnetworks  = undef,
+  $ibnetmask   = '255.255.252.0',
+  $ibhosts     = undef,
+  $ibinterface = 'ib0'
 ) {
-  if $vnic_interfaces != '' and $map_ib == 'true' {
-    $vnics = split($vnic_interfaces, ',')
+
+  if type($map_ib) == 'string' {
+    $map_ib_real = str2bool($map_ib)
+  } else {
+    $map_ib_real = $map_ib
+  }
+
+  validate_bool($map_ib_real)
+  validate_hash($ibnetworks)
+  validate_hash($ibhosts)
+  validate_string($ibnetmask) # TODO: replace with validate_re
+  validate_string($ibinterface)
+
+  if $::vnic_interfaces != '' and $map_ib_real == true {
+    $vnics = split($::vnic_interfaces, ',')
     infiniband::map_ib { $vnics:
-      ibnetmask => $ibnetmask,
-      ibnetworks => $ibnetworks,
+      ibnetmask   => $ibnetmask,
+      ibnetworks  => $ibnetworks,
       ibinterface => $ibinterface,
     }
   }
